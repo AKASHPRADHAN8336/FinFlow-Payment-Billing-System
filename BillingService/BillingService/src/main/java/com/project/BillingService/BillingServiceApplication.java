b87c27d9e2138013234ddd640585834cf624884e
AKASHPRADHAN8336 <your-email@example.com>
2025-12-09 00:17:04 +0530

    config server added in the project

--- BillingService/BillingService/src/main/java/com/project/BillingService/BillingServiceApplication.java
+++ BillingService/BillingService/src/main/java/com/project/BillingService/BillingServiceApplication.java
@@ -2,12 +2,14 @@ package com.project.BillingService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
 public class BillingServiceApplication {
 
 
--- WalletService/WalletService/src/main/java/com/project/WalletService/WalletServiceApplication.java
+++ WalletService/WalletService/src/main/java/com/project/WalletService/WalletServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WalletServiceApplication {
 
 	public static void main(String[] args) {
